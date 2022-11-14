40f38176e100f624ca920c68e7cc2af339cd371a
Arturas <a.minderis@gmail.com>
2022-11-14 12:54:47 +0200

    task3 code

--- JS Tasks/Task 3/script.js
+++ JS Tasks/Task 3/script.js
@@ -12,3 +12,48 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+document.getElementById('btn').addEventListener('click', updateForm);
+const output = document.getElementById('output');
+
+function updateForm() {
+    output.innerHTML = "";
+    showUsers();
+}
+
+async function showUsers() {
+    try {
+        await fetch(ENDPOINT)
+            .then(res => res.json())
+            .then(response => {
+                response.forEach((element, index) => {
+                    const container = newElement('div', 'id', 'container' + index, 'class', 'container');
+                    const userLogin = newElement('div', 'id', 'user' + index, 'class', 'userLogin');
+                    const userAvatar = newElement('div', 'id', 'avatar' + index, 'class', 'userAvatar');
+                    const avatarImg = newElement('img', 'id', 'image' + index, 'class', 'image');
+                    avatarImg.setAttribute('src', element.avatar_url);
+                    userLogin.innerHTML = element.login;
+                    userAvatar.appendChild(avatarImg);
+                    container.append(userLogin, userAvatar);
+                    output.appendChild(container);
+                });
+            })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// create new element: first argument = element name, all other key -> value pairs, class can be defined multiple times
+function newElement(...args) {
+    const element = document.createElement(args[0]);
+    for (let i = 1; i < args.length; i = i + 2) {
+        if (args[i] && args[i + 1]) {
+            if (args[i] == 'class') {
+                element.classList.add(args[i + 1]);
+            } else {
+                element.setAttribute(args[i], args[i + 1]);
+            }
+        }
+    }
+    return element;
+}
